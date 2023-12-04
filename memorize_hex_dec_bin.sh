e34adcaffd7c3cec7d0379b0bf8a2e25a2ae7b07
wenda_li <wdli@umich.edu>
2023-12-04 01:30:34 -0500

    upload

--- memorize_hex_dec_bin.sh
+++ memorize_hex_dec_bin.sh
@@ -5,3 +5,21 @@
 # numbers.
 # this script is intended to help me memorize the decimal form and binary form of A, B, C, D, E, F
 
+if [ $# -eq 0 ]
+then 
+  # user does not input file path
+  # default file path is the current directory
+  file="memorize_hex_dec_bin.md"
+  echo "Add fist item into file" > $file
+  echo "append second item into file" >> $file
+elif [ $# -eq 1 ]
+then
+  file="$1/memorize_hex_dec_bin.md"
+  echo "adding first" > $file
+  echo "append second" >> $file
+else
+  echo "error!"
+  exit 64
+fi
+
+cat $file
---
+++ test/memorize_hex_dec_bin.md
@@ -0,0 +1,2 @@
+adding first
+append second
